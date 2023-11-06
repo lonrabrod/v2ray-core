32812dadd90d4e5471a76084f2a9df5245c79b3f
lonrabrod <99519513+lonrabrod@users.noreply.github.com>
2023-11-06 08:26:03 -0800

    Update config.go

--- proxy/blackhole/config.go
+++ proxy/blackhole/config.go
@@ -1,25 +1,36 @@
 package blackhole
 
 import (
-	"github.com/v2fly/v2ray-core/v5/common"
-	"github.com/v2fly/v2ray-core/v5/common/buf"
-	"github.com/v2fly/v2ray-core/v5/common/serial"
+    "fmt"
+    "github.com/v2fly/v2ray-core/v5/common"
+    "github.com/v2fly/v2ray-core/v5/common/buf"
+    "github.com/v2fly/v2ray-core/v5/common/serial"
 )
 
 const (
-	http403response = `HTTP/1.1 403 Forbidden
+    http403response = `HTTP/1.1 403 Forbidden
 Connection: close
 Cache-Control: max-age=3600, public
-Content-Length: 0
+Content-Type: text/html
+Content-Length: %d
 
-
+<!DOCTYPE html>
+<html>
+<head>
+    <title>403 Forbidden</title>
+</head>
+<body>
+    <h1>403 Forbidden</h1>
+    <p>You do not have permission to access this resource.kevin</p>
+</body>
+</html>
 `
 )
 
 // ResponseConfig is the configuration for blackhole responses.
 type ResponseConfig interface {
-	// WriteTo writes predefined response to the give buffer.
-	WriteTo(buf.Writer) int32
+    // WriteTo writes predefined response to the given buffer.
+    WriteTo(buf.Writer) int32
 }
 
 // WriteTo implements ResponseConfig.WriteTo().
@@ -27,22 +38,23 @@ func (*NoneResponse) WriteTo(buf.Writer) int32 { return 0 }
 
 // WriteTo implements ResponseConfig.WriteTo().
 func (*HTTPResponse) WriteTo(writer buf.Writer) int32 {
-	b := buf.New()
-	common.Must2(b.WriteString(http403response))
-	n := b.Len()
-	writer.WriteMultiBuffer(buf.MultiBuffer{b})
-	return n
+    response := fmt.Sprintf(http403response, len(http403response))
+    b := buf.New()
+    common.Must2(b.WriteString(response))
+    n := b.Len()
+    writer.WriteMultiBuffer(buf.MultiBuffer{b})
+    return n
 }
 
-// GetInternalResponse converts response settings from proto to internal data structure.
+// GetInternalResponse converts response settings from proto to the internal data structure.
 func (c *Config) GetInternalResponse() (ResponseConfig, error) {
-	if c.GetResponse() == nil {
-		return new(NoneResponse), nil
-	}
+    if c.GetResponse() == nil {
+        return new(NoneResponse), nil
+    }
 
-	config, err := serial.GetInstanceOf(c.GetResponse())
-	if err != nil {
-		return nil, err
-	}
-	return config.(ResponseConfig), nil
+    config, err := serial.GetInstanceOf(c.GetResponse())
+    if err != nil {
+        return nil, err
+    }
+    return config.(ResponseConfig), nil
 }
